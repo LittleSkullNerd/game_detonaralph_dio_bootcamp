e8a720afcda0c078bd189b338cd7cefe9c431622
Luciana Sonoda <lulu.sonoda@hotmail.com>
2025-06-10 17:38:53 -0300

    add-game

---
+++ src/scripts/engine.js
@@ -0,0 +1,90 @@
+const state = {
+  view: {
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score"),
+  },
+  values: {
+    gameVelocity: 1000,
+    hitPosition: 0,
+    result: 0,
+    currentTime: 60,
+  },
+  actions: {
+    timerId: null,
+    countDownTimerId: null,
+  },
+};
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
+  state.values.hitPosition = randomSquare.id;
+}
+
+function playSound(audioName) {
+  let audio = new Audio(`./src/audios/${audioName}.m4a`);
+  audio.volume = 0.1;
+  audio.play();
+}
+
+function addListenerHitbox() {
+  state.view.squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === state.values.hitPosition) {
+        state.values.result++;
+        state.view.score.textContent = state.values.result;
+        state.values.hitPosition = null;
+        playSound("hit");
+      }
+    });
+  });
+}
+
+function countDown() {
+  state.values.currentTime--;
+  state.view.timeLeft.textContent = state.values.currentTime;
+
+  if (state.values.currentTime <= 0) {
+    clearInterval(state.actions.countDownTimerId);
+    clearInterval(state.actions.timerId);
+
+    playSound("gameover");
+
+    setTimeout(() => {
+      alert("Game Over! Seu resultado foi: " + state.values.result);
+      const playAgain = confirm("Deseja jogar novamente?");
+      if (playAgain) {
+        restartGame();
+      }
+    }, 500);
+  }
+}
+
+function restartGame() {
+  state.values.result = 0;
+  state.view.score.textContent = 0;
+  state.values.currentTime = 60;
+  state.view.timeLeft.textContent = 60;
+
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+
+  state.actions.timerId = setInterval(randomSquare, state.values.gameVelocity);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
+function initialize() {
+  addListenerHitbox();
+  state.actions.timerId = setInterval(randomSquare, state.values.gameVelocity);
+  state.actions.countDownTimerId = setInterval(countDown, 1000);
+}
+
+initialize();
